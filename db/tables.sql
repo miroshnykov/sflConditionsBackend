178ee5fe9b27f5a568383a24d1984bf18c7f1e20
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-04-06 16:57:39 -0400

    added new tables sfl_platform, sfl_source_type, sfl_advertiser_targeting

--- db/tables.sql
+++ db/tables.sql
@@ -31,3 +31,46 @@ CREATE TABLE `sfl_users` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
+
+CREATE TABLE `sfl_platform` (
+	`id` INT(10) UNSIGNED NOT NULL,
+	`name` VARCHAR(45) NOT NULL
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+INSERT INTO `sfl_platform` (`id`, `name`) VALUES (1, 'android');
+INSERT INTO `sfl_platform` (`id`, `name`) VALUES (2, 'windows');
+INSERT INTO `sfl_platform` (`id`, `name`) VALUES (3, 'ios');
+
+
+CREATE TABLE `sfl_source_type` (
+	`id` INT(10) UNSIGNED NOT NULL,
+	`name` VARCHAR(45) NOT NULL
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+INSERT INTO `sfl_source_type` (`id`, `name`) VALUES (1, 'sweepstakes');
+INSERT INTO `sfl_source_type` (`id`, `name`) VALUES (2, 'VOD');
+
+CREATE TABLE `sfl_advertiser_targeting` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`sfl_advertiser_campaign_id` INT(10) UNSIGNED NOT NULL,
+	`geo` VARCHAR(10) NOT NULL,
+	`platform` VARCHAR(10) NOT NULL DEFAULT '',
+	`source_type` VARCHAR(10) NOT NULL DEFAULT '',
+	`cpc` DECIMAL(16,8) NOT NULL,
+	`match_type_id` TINYINT(3) UNSIGNED NOT NULL,
+	`date_added` INT(11) NOT NULL,
+	`date_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	INDEX `sfl_advertiser_campaign_id` (`sfl_advertiser_campaign_id`),
+	CONSTRAINT `FK_sfl_advertiser_targeting_sfl_advertiser_campaigns` FOREIGN KEY (`sfl_advertiser_campaign_id`) REFERENCES `sfl_advertiser_campaigns` (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
