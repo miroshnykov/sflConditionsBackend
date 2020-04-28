ad669ec1ff887ddb1c7b91c0a5e7f1aae8a692f9
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-04-27 23:48:05 -0400

    add traffic_history table

---
+++ db/tables/traffic_history.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `sfl_traffic_history` (
+	`sfl_advertiser_campaign_id` INT(10) UNSIGNED NOT NULL,
+	`count_click` INT(10) UNSIGNED NOT NULL,
+	`date_by_days` DATE NOT NULL,
+	`date_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	UNIQUE INDEX `uk_sfl_advertiser_campaign_id` (`sfl_advertiser_campaign_id`, `date_by_days`),
+	INDEX `FK_sfl_sfl_traffic_history` (`sfl_advertiser_campaign_id`),
+	CONSTRAINT `FK_sfl_sfl_traffic_history` FOREIGN KEY (`sfl_advertiser_campaign_id`) REFERENCES `sfl_advertiser_campaigns` (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
