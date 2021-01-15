d75c4442164ded8a669b4c434cd4f8403bb4266e
miroshnykov <dmytro.miroshnykov@adsurge.com>
2021-01-14 20:47:52 -0500

    AL-488 some table

--- db/tables/offers.sql
+++ db/tables/offers.sql
@@ -20,7 +20,7 @@ CREATE TABLE `sfl_offers` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(128) NOT NULL,
 	`advertiser` VARCHAR(128) NOT NULL,
-	`status` ENUM('active','inactive') NOT NULL DEFAULT 'inactive',
+	`status` ENUM('public','private','apply_to_run','inactive') NOT NULL DEFAULT 'inactive',
 	`conversion_type` ENUM('cpi','cpa','cpl','cpc','cpm','hybrid/multistep') NOT NULL DEFAULT 'cpi',
 	`user` VARCHAR(50) NOT NULL DEFAULT '0',
 	`sfl_offer_landing_page_id` INT(11) NULL DEFAULT '0',
@@ -55,6 +55,63 @@ CREATE TABLE `sfl_offer_campaigns` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
+
+CREATE TABLE `sfl_offers_cap` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`sfl_offer_id` INT(10) UNSIGNED NOT NULL,
+	`clicks_day` INT(11) NOT NULL DEFAULT '0',
+	`clicks_week` INT(11) NOT NULL DEFAULT '0',
+	`clicks_month` INT(11) NOT NULL DEFAULT '0',
+	`clicks_redirect_status` ENUM('default','custom') NOT NULL DEFAULT 'default',
+	`clicks_redirect_offer_id` INT(10) UNSIGNED NOT NULL,
+	`sales_day` INT(11) NOT NULL DEFAULT '0',
+	`sales_week` INT(11) NOT NULL DEFAULT '0',
+	`sales_month` INT(11) NOT NULL DEFAULT '0',
+	`sales_redirect_status` ENUM('default','custom') NOT NULL DEFAULT 'default',
+	`sales_redirect_offer_id` INT(10) UNSIGNED NOT NULL,
+	`start_date` INT(11) NOT NULL,
+	`date_added` INT(11) NOT NULL,
+	`date_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `Index_sfl_offer_id` (`sfl_offer_id`),
+	INDEX `fk_redirect_offer_id` (`clicks_redirect_offer_id`),
+	INDEX `fk_sales_redirect_offer_id` (`sales_redirect_offer_id`),
+	CONSTRAINT `fk_clicks_redirect_offer_id` FOREIGN KEY (`clicks_redirect_offer_id`) REFERENCES `sfl_offers` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `fk_sales_redirect_offer_id` FOREIGN KEY (`sales_redirect_offer_id`) REFERENCES `sfl_offers` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `fk_sfl_offer_id` FOREIGN KEY (`sfl_offer_id`) REFERENCES `sfl_offers` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `sfl_offer_campaign_cap` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`sfl_offer_campaign_id` INT(10) UNSIGNED NOT NULL,
+	`clicks_day` INT(11) NOT NULL DEFAULT '0',
+	`clicks_week` INT(11) NOT NULL DEFAULT '0',
+	`clicks_month` INT(11) NOT NULL DEFAULT '0',
+	`clicks_redirect_status` ENUM('default','custom') NOT NULL DEFAULT 'default',
+	`clicks_redirect_offer_id` INT(10) UNSIGNED NOT NULL,
+	`sales_day` INT(11) NOT NULL DEFAULT '0',
+	`sales_week` INT(11) NOT NULL DEFAULT '0',
+	`sales_month` INT(11) NOT NULL DEFAULT '0',
+	`sales_redirect_status` ENUM('default','custom') NOT NULL DEFAULT 'default',
+	`sales_redirect_offer_id` INT(10) UNSIGNED NOT NULL,
+	`start_date` INT(11) NOT NULL,
+	`date_added` INT(11) NOT NULL,
+	`date_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `Index_sfl_offer_campaign_id` (`sfl_offer_campaign_id`),
+	INDEX `fk1_clicks_redirect_offer_id` (`clicks_redirect_offer_id`),
+	INDEX `fk1_sales_redirect_offer_id` (`sales_redirect_offer_id`),
+	CONSTRAINT `fk1_clicks_redirect_offer_id` FOREIGN KEY (`clicks_redirect_offer_id`) REFERENCES `sfl_offers` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `fk1_sales_redirect_offer_id` FOREIGN KEY (`sales_redirect_offer_id`) REFERENCES `sfl_offers` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `fk1_sfl_offer_id` FOREIGN KEY (`sfl_offer_campaign_id`) REFERENCES `sfl_offer_campaigns` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
 
 CREATE TABLE `sfl_offer_geo` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
